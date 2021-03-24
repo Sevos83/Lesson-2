6a9c28fccdc8695618b3397f6da7b71b6c987b10
Sevos83 <81201994+Sevos83@users.noreply.github.com>
2021-03-24 20:39:31 +0400

    Update lesson4.rb

--- lesson4.rb
+++ lesson4.rb
@@ -7,13 +7,13 @@ b = gets.chomp.to_i
 puts 'Enter c'
 c = gets.chomp.to_i
 
-d = b ** 2 - 4 * a * c
+d = b**2-4*a*c
 
 if d < 0
   puts "No roots"
 elsif d == 1
-  puts "Result is: #{-b / (2 * a)}"
+  puts "Result is: #{-b/(2*a)}"
 else
-  puts "x1 is: #{(-b + Math.sqrt(d)) / (2 * a)}"
-  puts "x2 is: #{(-b - Math.sqrt(d)) / (2 * a)}"
+  puts "x1 is: #{(-b+Math.sqrt(d))/(2*a)}"
+  puts "x2 is: #{(-b-Math.sqrt(d))/(2*a)}"
 end
