29eb291b6f687f73b597a60bf190e64036e7a2ce
Sevos83 <81201994+Sevos83@users.noreply.github.com>
2021-03-24 14:08:22 +0400

    Update lesson4.rb

--- lesson4.rb
+++ lesson4.rb
@@ -1,10 +1,10 @@
-puts "Enter a"
+puts 'Enter a'
 a = gets.chomp.to_i
 
-puts "Enter b"
+puts 'Enter b'
 b = gets.chomp.to_i
 
-puts "Enter c"
+puts 'Enter c'
 c = gets.chomp.to_i
 
 d = b ** 2 - 4 * a * c
